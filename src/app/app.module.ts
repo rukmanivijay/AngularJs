6b93dd0351e6b0b8aa391e68613e070bdf86ca92
Rukmani Vijayakumar <rukmani.vijaykumar@gmail.com>
2019-08-18 19:28:39 -0400

    Configured App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { MenuComponent } from './menu/menu.component';
     MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
