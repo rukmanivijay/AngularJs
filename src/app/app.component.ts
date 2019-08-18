9ec48f1ec676360917b558f3e72b68d27036d12d
Rukmani Vijayakumar <rukmani.vijaykumar@gmail.com>
2019-08-18 19:21:26 -0400

    2ndCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import 'hammerjs';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule
+  ]
+})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
